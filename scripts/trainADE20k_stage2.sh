63449441d43503ed7e87f7c163662d43e5aaa279
Franfee <599450863@qq.com>
2024-06-18 20:23:09 +0800

    📃 docs: update

--- scripts/trainADE20k_stage2.sh
+++ scripts/trainADE20k_stage2.sh
@@ -1,4 +1,4 @@
 torchrun --standalone --nproc_per_node=4 train.py \
 	--data="datasets/ADEChallengeData2016" --data_mode="ade20k" --resolution=256 \
 	--batch=3 --batch-gpu=768 \
-	--lr=1e-4 --dropout=0.13 --ls=1 --augment=0.2
+	--lr=1e-4 --dropout=0.13 --ls=1 --augment=0
